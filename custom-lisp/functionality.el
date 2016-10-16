44e8b0d49aa6fdbddd2d17ba983f2044306a89ab
Simon Ejdemyr <ejdemyr@stanford.edu>
2016-10-15 23:08:31 -0700

    scroll settings

--- custom-lisp/functionality.el
+++ custom-lisp/functionality.el
@@ -1,8 +1,13 @@
 
 
-; control scrolling speed
-(setq mouse-wheel-scroll-amount '(3 ((shift) . 1) ((control) . nil)))
-(setq mouse-wheel-progressive-speed nil)
+;; control scrolling speed
+(validate-setq
+ scroll-conservatively 1000          ; Never recenter the screen while scrolling
+ scroll-error-top-bottom t           ; Move to beg/end of buffer before signalling an error
+ ;; These settings make trackpad scrolling on OS X much more predictable
+ ;; and smooth
+ mouse-wheel-progressive-speed nil
+ mouse-wheel-scroll-amount '(3))
 
 ;; delete trailing whitespace before saving
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
@@ -12,9 +17,6 @@
 
 ;; delete selected text when typing
 (delete-selection-mode t)
-
-; switch between buffers using M-tab
-(global-set-key [M-tab] 'other-window)
 
 
 (provide 'functionality)
