1931b52c1f3efe8910b1bc2483a9cb7747031b9c
Simon Ejdemyr <ejdemyr@stanford.edu>
2016-10-18 11:18:33 -0700

    add custom lisp for final settings

---
+++ custom-lisp/final-settings.el
@@ -0,0 +1,9 @@
+;;; this script should be executed at the end as it requires some
+;;; packages (for now, only ESS) to be loaded
+
+
+;; find file and split screen on startup
+(split-window-right)
+(find-file "/Users/simonejdemyr/dropbox/scratch.R")
+
+(provide 'final-settings)
