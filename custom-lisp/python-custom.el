9fa62630be3d29d592f9e3e6d650aa115a92a1bc
Simon Ejdemyr <sejdemyr@netflix.com>
2020-11-25 14:50:44 -0800

    Support Python code navigation via jedi and pyvenv

--- custom-lisp/python-custom.el
+++ custom-lisp/python-custom.el
@@ -1,6 +1,24 @@
 (use-package ivy
   :ensure t)
 
+;; pyvenv manages virtual environments
+;; in this case we need it to specify from where to install the jedi server via `jedi:install-server`
+;; activate a virtualenv using `M-x pyenv-workon`
+(use-package pyvenv
+  :ensure t
+  :config
+  (pyvenv-mode 1)
+  (setenv "WORKON_HOME" "/Users/sejdemyr/xp-env/conda/envs/xp-env")
+  )
+
+;; jedi is used for code navigation (e.g., goto-definition)
+;; requires `jedi:install-server` (potentially after setting `M-x pyenv-workon`)
+;; further config in python-mode below (`my/python-company-jedi-config`)
+(use-package company-jedi
+  :ensure t
+  :defer)
+
+;; enable IDE type features
 (use-package elpy
   :ensure t
   :defer t
@@ -28,6 +46,15 @@
     (if (get-buffer "*Python*")
         (let ((kill-buffer-query-functions nil)) (kill-buffer "*Python*")))
     (elpy-shell-send-region-or-buffer))
+
+  (defun my/python-company-jedi-config ()
+    (set (make-local-variable 'company-backends) '(company-jedi))
+    (company-mode)
+    (local-set-key (kbd "s-p") 'jedi:goto-definition)
+    (local-set-key (kbd "s-P") 'jedi:goto-definition-pop-marker))
+
+  (add-hook 'python-mode-hook 'my/python-company-jedi-config)
+  )
   )
 
 
